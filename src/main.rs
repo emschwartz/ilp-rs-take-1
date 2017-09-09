0f511ec37133bfcc14cf4bbe6f79e2820c65ea4d
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-09-08 22:58:43 -0700

    feat: cli structure

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+extern crate clap;
+
+use clap::{App, SubCommand, Arg};
+
+fn main() {
+    let matches = App::new("spsp")
+        .version("0.1.0")
+        .author("Evan Schwartz <evan@ripple.com>")
+        .about("Command line sending client for ILP/SPSP")
+        .subcommand(SubCommand::with_name("quote")
+                    .about("Get a quote")
+                    .arg(Arg::with_name("source_amount")
+                         .takes_value(true)
+                         .long("source_amount")
+                         .required(true)
+                         .conflicts_with("destination_amount"))
+                    .arg(Arg::with_name("destination_amount")
+                         .takes_value(true)
+                         .long("destination_amount")
+                         .required(true)
+                         .conflicts_with("source_amount"))
+                    .arg(Arg::with_name("receiver")
+                        .index(1)
+                        .required(true)))
+        .subcommand(SubCommand::with_name("pay")
+                    .about("Send a payment")
+                    .arg(Arg::with_name("source_amount")
+                         .takes_value(true)
+                         .long("source_amount")
+                         .required(true))
+                    .arg(Arg::with_name("destination_amount")
+                         .takes_value(true)
+                         .long("destination_amount")
+                         .required(true))
+                    .arg(Arg::with_name("receiver")
+                        .index(1)
+                        .required(true)))
+        .get_matches();
+    match matches.subcommand_name() {
+        Some("quote") => {
+            let matches = matches.subcommand_matches("quote").unwrap();
+            let receiver = matches.value_of("receiver").unwrap();
+            if matches.is_present("source_amount") {
+                let source_amount = u64::from_str_radix(matches.value_of("source_amount").unwrap(), 10).unwrap();
+                let destination_amount = spsp::quote_source(receiver, source_amount);
+                println!("{}", destination_amount)
+            } else {
+                let destination_amount = u64::from_str_radix(matches.value_of("destination_amount").unwrap(), 10).unwrap();
+                let source_amount = spsp::quote_destination(receiver, destination_amount);
+                println!("{}", source_amount)
+            }
+        },
+        Some("pay") => {
+            let matches = matches.subcommand_matches("pay").unwrap();
+            let receiver = matches.value_of("receiver").unwrap();
+            let source_amount = u64::from_str_radix(matches.value_of("source_amount").unwrap(), 10).unwrap();
+            let destination_amount = u64::from_str_radix(matches.value_of("destination_amount").unwrap(), 10).unwrap();
+            spsp::pay(receiver, source_amount, destination_amount)
+        },
+        Some(command) => println!("unknown command: {}", command),
+        None => println!("command is required")
+    }
+}
+
+pub mod spsp {
+    pub fn quote_source(receiver: &str, source_amount: u64) -> u64 {
+        10
+    }
+
+    pub fn quote_destination(receiver: &str, destination_amount: u64) -> u64 {
+        10
+    }
+
+    pub fn pay(receiver: &str, source_amount: u64, destination_amount: u64) -> () {
+        println!("Send payment to {} with source amount: {} and destination amount: {}", receiver, source_amount, destination_amount);
+    }
+}
